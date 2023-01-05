d7a18d1bda219dd332c012cab8cc2b7e73604197
Ahranna Araujo <ahrannaaraujo@ahrannas-air.lan>
2023-01-05 18:25:15 +0000

    update functionality created

--- src/controllers/posts.controllers.js
+++ src/controllers/posts.controllers.js
@@ -26,3 +26,30 @@ export async function deletePost(req, res) {
    }
     
 }
+
+export async function updatePost(req, res) {
+    const { id } = req.params
+    const { description } = req.body
+   try {
+     const post = await connection.query(`SELECT * FROM posts WHERE id = $1`,[id] )
+     console.log(post)
+
+    if(post.rows.length == 0){
+        res.sendStatus(404)
+        return
+    }
+
+    if(post.rows[0].userId == res.locals.userId) {
+        await connection.query(`UPDATE posts SET description = $1  WHERE id = $2`, [description, id])
+        res.sendStatus(201)
+     } else {
+        res.status(401).send("Esse usuário não é permitido")
+     }
+     
+    } catch (err){
+      console.log(err)
+      res.sendStatus(500)
+   }
+
+   return
+}
--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express"
-import { deletePost } from "../controllers/posts.controllers.js"
+import { deletePost, updatePost } from "../controllers/posts.controllers.js"
 import { authRoutesValidation } from "../middlewares/auth.Validation.middlewares.js"
 
 const postsRoutes = Router()
 
-postsRoutes.delete("/posts/:id",authRoutesValidation, deletePost)
+postsRoutes.delete("/posts/:id",authRoutesValidation,deletePost)
+postsRoutes.put("/posts/:id",authRoutesValidation,updatePost)
 
 export default postsRoutes
