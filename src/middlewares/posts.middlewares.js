82ec77b9eed356244715668d26db0736ebe6995e
Júlia Botelho <cajujumo@gmail.com>
2023-01-06 15:13:44 -0300

    feat: timeline basic functions

--- src/middlewares/posts.middlewares.js
+++ src/middlewares/posts.middlewares.js
@@ -2,10 +2,12 @@ import { postSchema } from "../Schemas/post.schema.js"
 
 export async function postValidation(req, res, next) {
     const post = req.body;
-    const { error } = postSchema.validate(user, { abortEarly: false });
+
+    const { error } = postSchema.validate(post, { abortEarly: false });
     if (error) {
         const errors = error.details.map((detail) => detail.message);
-        res.status(422).send(errors);
-        return;
+        return res.status(422).send(errors);
     }
+
+    next();
 }
