08f4e726f2db5c6105e71efba8629179586ca6b9
RodrigoCosta <costardgg@gmail.com>
2023-01-12 15:29:17 -0300

    feat:validate Authorization and Fix: query getPostHashtag

--- src/routes/hashtags.routes.js
+++ src/routes/hashtags.routes.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import { getPostHashtag, getRankingHashtags, openHashtag } from "../controllers/hashtags.controller.js";
-
+import {
+  getPostHashtag,
+  getRankingHashtags,
+  openHashtag,
+} from "../controllers/hashtags.controller.js";
+import { authRoutesValidation } from "../middlewares/auth.Validation.middlewares.js";
 const router = Router();
 
-router.get("/hashtags",getRankingHashtags)
-router.get("/hashtags/:hashtag",openHashtag)
-router.get("/post/:hashtag",getPostHashtag)
+router.get("/hashtags",authRoutesValidation, getRankingHashtags);
+router.get("/hashtags/:hashtag",authRoutesValidation, openHashtag);
+router.get("/post/:hashtag",authRoutesValidation, getPostHashtag);
 
 export default router;
