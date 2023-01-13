f29b8f5d703c7b26efe9afdf3b06fc23c8524e34
Júlia Botelho <cajujumo@gmail.com>
2023-01-13 18:08:36 -0300

    fix: dump and repost funtional and conncention string

--- src/database/server.js
+++ src/database/server.js
@@ -5,11 +5,8 @@ const { Pool } = pkg;
 dotenv.config();
 
 export const connection = new Pool({
-  host:'localhost',
-  port: 5432,
-  user: 'postgres',
-  password: 'root',
-  database: 'linkr'
+  connectionString: process.env.DATABASE_URL ,
+  ssl : true
 });
 
 /* {connectionString: process.env.DATABASE_URL /* 
